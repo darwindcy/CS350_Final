93e6f7937fed71d0c70529ceba7fe8bafae13ab4
Darwin Charles Yadav <darwindcy@gmail.com>
2020-04-20 13:14:09 -0500

    Made homepage according to design

--- Blogging Away/Home.Designer.cs
+++ Blogging Away/Home.Designer.cs
@@ -28,78 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabHome = new System.Windows.Forms.TabPage();
+            this.tabMessageList = new System.Windows.Forms.TabPage();
             this.txtScreen = new System.Windows.Forms.TextBox();
             this.btnCreateNew = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.tabAbout = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.btnLogout = new System.Windows.Forms.Button();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabHome.SuspendLayout();
+            this.tabMessageList.SuspendLayout();
+            this.tabAbout.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.btnLogout);
-            this.panel1.Controls.Add(this.txtScreen);
-            this.panel1.Controls.Add(this.btnCreateNew);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(152, 22);
+            this.panel1.Controls.Add(this.tabControl1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(67, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(499, 377);
+            this.panel1.Size = new System.Drawing.Size(795, 609);
             this.panel1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.tabControl1.Controls.Add(this.tabHome);
+            this.tabControl1.Controls.Add(this.tabMessageList);
+            this.tabControl1.Controls.Add(this.tabAbout);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.ImageList = this.imageList1;
+            this.tabControl1.ItemSize = new System.Drawing.Size(30, 120);
+            this.tabControl1.Location = new System.Drawing.Point(0, 71);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(704, 538);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 5;
+            this.tabControl1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.tabControl1_DrawItem);
+            // 
+            // tabHome
+            // 
+            this.tabHome.Controls.Add(this.label5);
+            this.tabHome.Location = new System.Drawing.Point(124, 4);
+            this.tabHome.Name = "tabHome";
+            this.tabHome.Padding = new System.Windows.Forms.Padding(3);
+            this.tabHome.Size = new System.Drawing.Size(576, 530);
+            this.tabHome.TabIndex = 0;
+            this.tabHome.Text = "Home";
+            this.tabHome.UseVisualStyleBackColor = true;
+            // 
+            // tabMessageList
+            // 
+            this.tabMessageList.Controls.Add(this.txtScreen);
+            this.tabMessageList.Controls.Add(this.btnCreateNew);
+            this.tabMessageList.Controls.Add(this.label1);
+            this.tabMessageList.Location = new System.Drawing.Point(124, 4);
+            this.tabMessageList.Name = "tabMessageList";
+            this.tabMessageList.Padding = new System.Windows.Forms.Padding(3);
+            this.tabMessageList.Size = new System.Drawing.Size(576, 530);
+            this.tabMessageList.TabIndex = 1;
+            this.tabMessageList.Text = "Message List";
+            this.tabMessageList.UseVisualStyleBackColor = true;
             // 
             // txtScreen
             // 
             this.txtScreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtScreen.Location = new System.Drawing.Point(99, 60);
+            this.txtScreen.Location = new System.Drawing.Point(30, 82);
             this.txtScreen.Multiline = true;
             this.txtScreen.Name = "txtScreen";
             this.txtScreen.ReadOnly = true;
             this.txtScreen.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtScreen.Size = new System.Drawing.Size(307, 257);
-            this.txtScreen.TabIndex = 2;
+            this.txtScreen.Size = new System.Drawing.Size(491, 445);
+            this.txtScreen.TabIndex = 8;
             // 
             // btnCreateNew
             // 
             this.btnCreateNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCreateNew.Location = new System.Drawing.Point(154, 333);
+            this.btnCreateNew.Location = new System.Drawing.Point(194, 550);
             this.btnCreateNew.Name = "btnCreateNew";
             this.btnCreateNew.Size = new System.Drawing.Size(187, 31);
-            this.btnCreateNew.TabIndex = 1;
+            this.btnCreateNew.TabIndex = 7;
             this.btnCreateNew.Text = "Create New Article";
             this.btnCreateNew.UseVisualStyleBackColor = true;
-            this.btnCreateNew.Click += new System.EventHandler(this.btnCreateNew_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(153, 17);
+            this.label1.Location = new System.Drawing.Point(201, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(193, 31);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Blogging Away";
+            this.label1.Size = new System.Drawing.Size(174, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Message List";
+            // 
+            // tabAbout
+            // 
+            this.tabAbout.Controls.Add(this.label3);
+            this.tabAbout.Controls.Add(this.label2);
+            this.tabAbout.Location = new System.Drawing.Point(124, 4);
+            this.tabAbout.Name = "tabAbout";
+            this.tabAbout.Size = new System.Drawing.Size(576, 530);
+            this.tabAbout.TabIndex = 2;
+            this.tabAbout.Text = "About";
+            this.tabAbout.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "#Something";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(238, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 31);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "About";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "home.png");
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(704, 71);
+            this.panel3.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnLogout);
+            this.panel2.Controls.Add(this.lblUser);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(704, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(91, 609);
+            this.panel2.TabIndex = 6;
             // 
             // btnLogout
             // 
             this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogout.Location = new System.Drawing.Point(383, 18);
+            this.btnLogout.Location = new System.Drawing.Point(13, 60);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(115, 29);
+            this.btnLogout.Size = new System.Drawing.Size(75, 32);
             this.btnLogout.TabIndex = 3;
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = true;
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.Location = new System.Drawing.Point(21, 9);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(43, 20);
+            this.lblUser.TabIndex = 4;
+            this.lblUser.Text = "User";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(265, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(193, 31);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Blogging Away";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(175, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 31);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Home";
+            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(912, 643);
             this.Controls.Add(this.panel1);
             this.Name = "Home";
             this.Text = "Home";
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabHome.ResumeLayout(false);
+            this.tabHome.PerformLayout();
+            this.tabMessageList.ResumeLayout(false);
+            this.tabMessageList.PerformLayout();
+            this.tabAbout.ResumeLayout(false);
+            this.tabAbout.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -107,9 +264,21 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnCreateNew;
-        private System.Windows.Forms.TextBox txtScreen;
         private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabHome;
+        private System.Windows.Forms.TabPage tabMessageList;
+        private System.Windows.Forms.TextBox txtScreen;
+        private System.Windows.Forms.Button btnCreateNew;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabAbout;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
--- Blogging Away/Home.cs
+++ Blogging Away/Home.cs
@@ -18,6 +18,8 @@ namespace Blogging_Away
         {
             InitializeComponent();
             RetrieveData();
+            String newLabel = lblUser.Text + Environment.NewLine + Login.currentUser;
+            lblUser.Text = newLabel;
             if (Login.currentUser != "admin" )
                 btnCreateNew.Visible = false;
         }
@@ -57,6 +59,40 @@ namespace Blogging_Away
             this.Hide();
             Login newLogin = new Login();
             newLogin.Show();
+        }
+
+        private void tabControl1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Brush _textBrush;
+
+            // Get the item from the collection.
+            TabPage _tabPage = tabControl1.TabPages[e.Index];
+
+            // Get the real bounds for the tab rectangle.
+            Rectangle _tabBounds = tabControl1.GetTabRect(e.Index);
+
+            if (e.State == DrawItemState.Selected)
+            {
+
+                // Draw a different background color, and don't paint a focus rectangle.
+                _textBrush = new SolidBrush(Color.Red);
+                g.FillRectangle(Brushes.Gray, e.Bounds);
+            }
+            else
+            {
+                _textBrush = new System.Drawing.SolidBrush(e.ForeColor);
+                e.DrawBackground();
+            }
+
+            // Use our own font.
+            Font _tabFont = new Font("Microsoft Sans Serif", (float)13.0, FontStyle.Regular, GraphicsUnit.Pixel);
+
+            // Draw string. Center the text.
+            StringFormat _stringFlags = new StringFormat();
+            _stringFlags.Alignment = StringAlignment.Center;
+            _stringFlags.LineAlignment = StringAlignment.Center;
+            g.DrawString(_tabPage.Text, _tabFont, _textBrush, _tabBounds, new StringFormat(_stringFlags));
         }
     }
 }
